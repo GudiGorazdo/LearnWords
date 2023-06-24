520748bd7d35ac93b46babdcd9c488ceb1231f3c
oseminikhin <oseminikhin@tolkit.top>
2023-06-25 00:19:44 +0300

    font awesome added; button icon added

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,40 +1,53 @@
 import React from 'react';
 import type { PropsWithChildren } from 'react';
 import { Text, StyleSheet, TouchableNativeFeedback, View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export type IButtonProps = PropsWithChildren<{
-  title: string,
-  onPress: () => void,
+	title: string,
+	onPress: () => void,
+	icon?: {
+		front: boolean,
+		type: string,
+		styles?: object, 
+	},
 }>;
 
-export function Button({ title, onPress }: IButtonProps) {
-  const handlePress = () => {
-    onPress();
-  };
+export function Button({ title, onPress, icon = undefined }: IButtonProps) {
+	const handlePress = () => {
+		onPress();
+	};
 
-  return (
-    <TouchableNativeFeedback onPress={handlePress}>
-      <View style={styles.button}>
-        <Text style={styles.buttonText}>{title}</Text>
-      </View>
-    </TouchableNativeFeedback>
-  );
+	const getIcon = () => {
+		if (!icon) return;
+		return <Icon name={icon.type} size={16} style={icon.styles ?? {}} />
+	}
+
+	return (
+		<TouchableNativeFeedback onPress={handlePress}>
+			<View style={styles.button}>
+				{icon && icon.front && getIcon()}
+				<Text style={styles.buttonText}>{title}</Text>
+				{icon && !icon.front && getIcon()}
+			</View>
+		</TouchableNativeFeedback>
+	);
 }
 
 const styles = StyleSheet.create({
-  button: {
+	button: {
 		width: '100%',
-    backgroundColor: 'blue',
-    borderRadius: 8,
-    padding: 10,
-    overflow: 'hidden',
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 16,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
+		backgroundColor: 'blue',
+		borderRadius: 8,
+		padding: 10,
+		overflow: 'hidden',
+	},
+	buttonText: {
+		color: 'white',
+		fontSize: 16,
+		fontWeight: 'bold',
+		textAlign: 'center',
+	},
 });
 
 
---
+++ src/modules/Header/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { NavigationProp } from '@react-navigation/native';
+import { Button } from '../../components/Button';
+
+import S_container from '../../styles/container';
+
+import {
+	SafeAreaView,
+	View,
+	StyleSheet,
+} from 'react-native';
+
+interface IHomeScreenProps {
+	navigation: NavigationProp<any>,
+}
+
+export function Add ({ navigation }: IHomeScreenProps): JSX.Element {
+	return (
+		<SafeAreaView>
+			<View style={styles.section}>
+				<Button title='Назад' onPress={() => navigation.navigate('Home')} />
+			</View>
+		</SafeAreaView>
+	);
+}
+
+const styles = StyleSheet.create({
+	section: {
+		flex: 1,
+		justifyContent: 'center',
+		rowGap: 20,
+		...S_container
+	},
+});
+
+
