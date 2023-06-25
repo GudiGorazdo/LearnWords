e2b9aff23383fb9e6b3e0e0e5d6dca6c424bb0bd
oseminikhin <oseminikhin@tolkit.top>
2023-06-25 22:07:52 +0300

    sqlite3 removed

--- src/storage/words.service.ts
+++ src/storage/words.service.ts
@@ -1,16 +1,12 @@
-import sqlite3 from 'sqlite3';
 import IConfig from '../config/config.interface';
 import SConfig from '../config/config.service';
 import ISwords from './words.service';
 
 export default class SWords implements ISwords {
 	private static instance: ISwords;
-	// private source: sqlite3.Database;
 
 	constructor() {
 		const config: IConfig = SConfig.getInstance();
-		// this.source = new sqlite3.Database(config.get('databaseName'));
-		// console.log(this.source);
 		this.init();
 	}
 
