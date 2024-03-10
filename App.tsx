f64b3823364a930afb9ca04d52e545d28dd44224
OSSemenikhin <semenikhinoleg777@yandex.ru>
2024-03-10 07:58:36 +0300

    icons; theme

--- App.tsx
+++ App.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import { StatusBar, useColorScheme } from 'react-native';
 import Navigation from './src/navigation/Navigation';
+import { Appearance } from 'react-native';
 // import type {PropsWithChildren} from 'react';
 //
 
 function App(): JSX.Element {
-	const isDarkMode = useColorScheme() === 'dark';
+  Appearance.setColorScheme('light');
+  // const isDarkMode = useColorScheme() === 'dark';
 
-	return (
-		<>
-			<StatusBar
-				translucent
-				barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-				backgroundColor="transparent"
-			/>
-			<Navigation />
-		</>
-	);
+  return (
+    <>
+      <StatusBar
+        translucent
+        // barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor="transparent"
+      />
+      <Navigation />
+    </>
+  );
 }
 
 
